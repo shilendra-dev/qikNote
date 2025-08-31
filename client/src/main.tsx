0f7cc1333f75808c85547e2283b081d2c8b6cbf9
Shilendra Singh <business.shilendra@gmail.com>
2025-08-31 09:08:46 +0530

    setup: generouted folder based auto routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Routes } from '@generouted/react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Routes />
   </StrictMode>,
 )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import generouted from "@generouted/react-router/plugin"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), generouted()],
 })
